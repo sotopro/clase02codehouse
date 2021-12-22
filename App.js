77451eed17bd04b17fccb26d9b5cc20f0b47acf3
Daniel Soto <dasg.1993@gmail.com>
2021-12-21 20:18:48 -0500

    [featute

--- App.js
+++ App.js
@@ -11,24 +11,38 @@ import {
   Text,
   View,
   TextInput,
-  Button
+  Button,
+  StyleSheet,
 } from 'react-native';
 
 
 
 const App = () => {
   return (
-    <View style={{ flex: 1}}>
-      <View style={{ flexDirection: 'row',  padding: 20, justifyContent: 'space-around', alignItems: 'center'}}>
-        <TextInput style={{ flex: 0.8, borderBottomWidth: 1, borderBottomColor: '#cccccc' }} placeholder='name a task'/>
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <TextInput style={styles.textInput} placeholder='name a task'/>
         <Button  title='Send' color='#9191E9' />
       </View>
     </View>
   );
 };
 
-// const styles = StyleSheet.create({
-  
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  formContainer: {
+    flexDirection: 'row', 
+    padding: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  textInput : {
+    flex: 0.8,
+    borderBottomWidth: 1, 
+    borderBottomColor: '#cccccc',
+  }
+})
 
 export default App;
