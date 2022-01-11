ddbeae2a5f09a6652fbf2bc1cb2f45bc687481cd
Daniel Soto <dasg.1993@gmail.com>
2022-01-11 18:44:42 -0500

    [feature] - handle buttons

--- App.js
+++ App.js
@@ -20,19 +20,36 @@ import Card from './src/components/molecules/card/index';
 import { theme } from './src/utils/constants/theme';
 import { components } from './src/utils/constants/components';
 const App = () => {
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState('');
 
   const handleOnChange = (inputNumber) => {
     console.warn(inputNumber)
     setNumber(inputNumber);
   }
 
+  const handleOnClean = () => {
+    setNumber('');
+  }
+  const handleOnConfirm = () => {
+    console.warn('Confirmar')
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Header title="Adivina el número" />
         <Title text="Comienza el juego" />
-        <Card title="Elija un número" type={components.card.LIGHT} color={theme.primaryColor} handleOnChange={handleOnChange} value={number} autoFocus={true} autoComplete='off' keyboardType='numeric'/>
+        <Card 
+          title="Elija un número"
+          type={components.card.LIGHT}
+          color={theme.primaryColor}
+          handleOnChange={handleOnChange} 
+          value={number} 
+          autoFocus={true} 
+          autoComplete='off' 
+          keyboardType='numeric'
+          handleOnClean={handleOnClean}
+          handleOnConfirm={handleOnConfirm}
+        />
       </View>
     </SafeAreaView>
   );
--- src/components/molecules/card/index.js
+++ src/components/molecules/card/index.js
@@ -4,16 +4,14 @@ import { theme } from '../../../utils/constants/theme';
 import { styles } from './styles';
 import Input from '../../atoms/input';
 
-const Card = ({title, color, type, handleOnChange, value, autoFocus,
-    autoComplete,
-    keyboardType}) => {
+const Card = ({title, color, type, handleOnChange, value, autoFocus, autoComplete, keyboardType, handleOnClean ,handleOnConfirm}) => {
     return (
         <View style={[styles.container, {backgroundColor: type === '1' ? theme.primaryVariant : theme.secondaryColor}]}>
             <Text style={styles.text}>{title}</Text>
             <Input autoFocus={autoFocus} autoComplete={autoComplete} keyboardType={keyboardType} onChangeText={handleOnChange} value={value} />
             <View style={styles.buttonContainer}>
-                <Button title="Limpiar" color={color} />
-                <Button title="Confirmar" color={color} />
+                <Button title="Limpiar" color={color} onPress={() => handleOnClean()}/>
+                <Button title="Confirmar" color={color} onPress={() => handleOnConfirm()}/>
             </View>
         </View>
     )
